8777be82b904a0e23b9cd1d2468e3e40b72586f9
Juan Herrera <herrera@parkside.at>
2020-05-03 17:22:13 +0200

    build: setup firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCGad5H4l_d4jBlszYk8LupNstx06dcbl8",
+    authDomain: "pr-memes.firebaseapp.com",
+    databaseURL: "https://pr-memes.firebaseio.com",
+    projectId: "pr-memes",
+    storageBucket: "pr-memes.appspot.com",
+    messagingSenderId: "172215964421",
+    appId: "1:172215964421:web:c4119ce27a5460c1de9d6c",
+    measurementId: "G-2EW7THDJ9N",
+  },
 };
 
 /*
